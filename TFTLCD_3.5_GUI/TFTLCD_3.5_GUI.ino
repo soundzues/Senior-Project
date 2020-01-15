67d8eafcb8f9af7cfd7955d7df8f9879d70970c0
Aditya Kothari <kotharad@oregonstate.edu>
2020-01-14 20:31:14 -0800

    Fixed from Portrait to Landscape, added 3 OPTs

--- TFTLCD_3.5_GUI/TFTLCD_3.5_GUI.ino
+++ TFTLCD_3.5_GUI/TFTLCD_3.5_GUI.ino
@@ -13,22 +13,43 @@ MCUFRIEND_kbv tft;
 #define YELLOW  0xFFE0
 #define WHITE   0xFFFF
 
-const int XP = 6, XM = A2, YP = A1, YM = 7; //ID=0x9341
+const int XP = 6, XM = A2, YP = A1, YM = 7; //TFT LCD Controling pins
 const int TS_LEFT = 907, TS_RT = 136, TS_TOP = 942, TS_BOT = 139;
 
 TouchScreen ts = TouchScreen(XP, YP, XM, YM, 300);
-
 
 void setup() {
     Serial.begin(9600);
     uint16_t ID = tft.readID();
     if (ID == 0xD3D3) ID = 0x9486; // write-only shield
     tft.begin(ID);
-    tft.setRotation(0);            //PORTRAIT
+    tft.setRotation(3);            //Landscape 270 mode
+
+    //Black Background
     tft.fillScreen(BLACK);
-    tft.fillRect(210, 0, 105, 480, RED);
-    tft.fillRect(105, 0, 105, 480, BLUE);
-    tft.fillRect(0, 0, 105, 480, GREEN);
+
+    //3 Option Boxes
+    tft.fillRect(0, 210, 480, 105, RED);  //button for OPT 3
+    tft.fillRect(0, 105, 480, 105, BLUE); //button for OPT 2
+    tft.fillRect(0, 0, 480, 105, GREEN);  //button for OPT 1
+
+    //Print Text for buttons
+    tft.setCursor(190,40);
+    tft.setTextSize(4);
+    tft.setTextColor(BLACK);
+    tft.println("OPT1");
+
+    tft.setCursor(190,145);
+    tft.setTextSize(4);
+    tft.setTextColor(BLACK);
+    tft.println("OPT2");
+
+
+    tft.setCursor(190,245);
+    tft.setTextSize(4);
+    tft.setTextColor(BLACK);
+    tft.println("OPT3");
+     
 }
 
 void loop() {
